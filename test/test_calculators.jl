1853f47a7246341e9c1015411b30258a052a238a
Christoph Ortner <c.ortner@warwick.ac.uk>
2018-05-25 08:31:50 +0100

    removed EMT test

--- test/test_calculators.jl
+++ test/test_calculators.jl
@@ -15,19 +15,19 @@ catch
 end
 
 
-println("Test ASE's EMT calculator implementation (ASE.EMTCalculator)")
-emt = ASE.EMTCalculator()
-at = rattle!( set_pbc!( bulk("Cu", cubic=true) * 2, (true,false,false) ), 0.1 )
-@test JuLIP.Testing.fdtest(emt, at, verbose=true)
-
-println("Test ASE+JuLIP implementation of EMT")
-emtj = ASE.EMT.EMTCalculator(at)
-
-println("--------------------------------------------------")
-println(" EMT Consistency test: ")
-println("--------------------------------------------------")
-println(" E_ase - E_jl = ", energy(emt, at) - energy(emtj, at))
-println(" |Frc_ase - Frc_jl = ", maximum(norm.(forces(emt, at) - forces(emtj, at))))
-println("--------------------------------------------------")
-@test abs(energy(emt, at) - energy(emtj, at)) < 1e-10
-@test JuLIP.Testing.fdtest(emtj, at, verbose=true)
+# println("Test ASE's EMT calculator implementation (ASE.EMTCalculator)")
+# emt = ASE.EMTCalculator()
+# at = rattle!( set_pbc!( bulk("Cu", cubic=true) * 2, (true,false,false) ), 0.1 )
+# @test JuLIP.Testing.fdtest(emt, at, verbose=true)
+#
+# println("Test ASE+JuLIP implementation of EMT")
+# emtj = ASE.EMT.EMTCalculator(at)
+#
+# println("--------------------------------------------------")
+# println(" EMT Consistency test: ")
+# println("--------------------------------------------------")
+# println(" E_ase - E_jl = ", energy(emt, at) - energy(emtj, at))
+# println(" |Frc_ase - Frc_jl = ", maximum(norm.(forces(emt, at) - forces(emtj, at))))
+# println("--------------------------------------------------")
+# @test abs(energy(emt, at) - energy(emtj, at)) < 1e-10
+# @test JuLIP.Testing.fdtest(emtj, at, verbose=true)
